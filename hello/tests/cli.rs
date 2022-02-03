a4c02423a4afcfcf4f210eedb4f51ad869cb8251
CJ Harries <cj@wizardsoftheweb.pro>
2022-02-03 13:19:10 -0600

    Update hello test to verify output

--- hello/tests/cli.rs
+++ hello/tests/cli.rs
@@ -3,5 +3,5 @@ use assert_cmd::Command;
 #[test]
 fn runs() {
     let mut cmd = Command::cargo_bin("hello").unwrap();
-    cmd.assert().success();
+    cmd.assert().success().stdout("Hello, world!\n");
 }
